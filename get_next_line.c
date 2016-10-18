9a54e5b3bb1a092856562837da99c351aaf765ac
robert passafaro <robert.passafaro@gmail.com>
2016-10-18 14:04:41 -0700

    passing 10 of 21 mouli test still in progress

--- get_next_line.c
+++ get_next_line.c
@@ -7,47 +7,64 @@
 # include "get_next_line.h"
 # include "./libft/libft.h"
 
-char	*read_tmp(const int fd, char *buf, int *bytes)
-{
-	char *str1;
+// read_buffer
+// buffer
 
-	str1 = ft_strnew(BUFF_SIZE);
-	*bytes = read(fd, str1, BUFF_SIZE);
-	str1[*bytes] = '\0';
-	buf = ft_strjoin(buf,(const char*)str1);
-	return buf;
+int read_tmp(const int fd, char **buf)
+{
+	char	str1[BUFF_SIZE + 1];
+	//char    *temp;
+	int		bytes_read;
+
+	bytes_read = 1;
+	while (bytes_read > 0 && ft_strchr(*buf, '\n') == NULL)
+	{
+		bytes_read = read(fd, &str1, BUFF_SIZE);
+		if (bytes_read < 0)
+			return (-1);
+		str1[bytes_read] = '\0';
+		*buf = ft_strjoin(*buf,(const char*)str1);
+		ft_bzero(str1, BUFF_SIZE);
+	}
+	return (bytes_read);
 }
 
 int get_next_line(const int fd, char **line)
 {
-	//**line is where the 
-	static char *buf = "";
-	char *str;
-	int	bytes;
+	//**line is where the
+	static char *buf;
+	char 		*str;
+	int			bytes_read;
+
 	if(fd < 0)
-
-	bytes = 1;
-	while (bytes > 0)
+		return (-1);
+	if (line == NULL || *line == NULL)
+		return (-1);
+	if (!buf)
+		buf = ft_strnew(BUFF_SIZE);
+	bytes_read = read_tmp(fd, &buf);
+	if((str = ft_strchr(buf, '\n')))
 	{
-		if ((str = ft_strchr(buf, '\n')) != NULL)
-		{
-			str[0] = '\0';
-			*line = ft_strdup(buf);
-			buf = str + 1;
-			return 1;
-		}
-		buf = read_tmp(fd, buf, &bytes);
+		str[0] = '\0';
+		*line = ft_strdup(buf);
+		buf = str + 1;
 	}
-	return 0;
+	else
+	{
+		*line = ft_strdup(buf);
+	}
+	if (bytes_read == 0)
+		return (0);
+	else
+		return (1);
 }
 
-int		main(int argc, char **argv)
-{
-	if(argc > 2)
-		return 0;
-	int fd = open(argv[1], O_RDWR);
-	char *line;
-	while(get_next_line(fd, &line))
-		printf("%s\n", line);
-
-}
+// int		main(int argc, char **argv)
+// {
+// 	if(argc > 2)
+// 		return 0;
+// 	int fd = open(argv[1], O_RDWR);
+// 	char *line = ft_strnew(0);
+// 	while(get_next_line(fd, &line))
+// 		printf("%s\n", line);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -2,9 +2,9 @@
 # define GET_NEXT_LINE_H
 
 # define ENDL '\n'
-# define BUFF_SIZE 999999
+# define BUFF_SIZE 30
 
 int get_next_line(const int fd, char **line);
-char *read_tmp(const int fd, char *buf, int *bytes);
+int	read_tmp(const int fd, char **buf);
 
 #endif
