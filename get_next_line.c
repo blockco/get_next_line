16d73c872615b0acd90a3bd97efceccc8d8ef871
robert passafaro <robert.passafaro@gmail.com>
2016-11-16 20:36:37 -0800

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/31 14:09:54 by rpassafa          #+#    #+#             */
+/*   Updated: 2016/10/31 15:11:12 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include "get_next_line.h"
-# include "./libft/libft.h"
+#include "get_next_line.h"
+#include "./libft/libft.h"
 
-int read_tmp(const int fd, char **buf)
+int	read_tmp(const int fd, char **buf)
 {
 	char	str1[BUFF_SIZE + 1];
 	int		bytes_read;
@@ -14,16 +25,16 @@ int read_tmp(const int fd, char **buf)
 		if (bytes_read < 0)
 			return (-1);
 		str1[bytes_read] = '\0';
-		*buf = ft_strjoin(*buf,(const char*)str1);
+		*buf = ft_strjoin(*buf, (const char*)str1);
 		ft_bzero(str1, BUFF_SIZE);
 	}
 	return (bytes_read);
 }
 
-int get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
 	static char *buf;
-	char 		*str;
+	char		*str;
 	int			bytes_read;
 
 	if (fd < 0 || line == NULL || (-1 == read(fd, buf, 0)))
@@ -38,7 +49,7 @@ int get_next_line(const int fd, char **line)
 		str[0] = '\0';
 		*line = ft_strdup(buf);
 		buf = str + 1;
-		return 1;
+		return (1);
 	}
 	*line = ft_strdup(buf);
 	buf = NULL;
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/31 15:10:36 by rpassafa          #+#    #+#             */
+/*   Updated: 2016/10/31 23:16:35 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -8,7 +20,7 @@
 # include <sys/types.h>
 
 # define ENDL '\n'
-# define BUFF_SIZE 30
+# define BUFF_SIZE 1
 
 int get_next_line(const int fd, char **line);
 int	read_tmp(const int fd, char **buf);
